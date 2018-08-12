2c2f20a3c3f79d81b667a4c81ceae9d31307af8a
98081767 <98081767@student.uts.edu.au>
2018-08-12 18:06:29 +1000

    New

---
+++ WebScraping.R
@@ -0,0 +1,8 @@
+#-----------------------------------------------------
+# Statistical Thinking - Assignment 1
+# Archel Aguilar - 98081767
+# Web scrapting of movie sales - from https://www.boxofficemojo.com/weekly/chart/?yr=2018&wk=31&p=.htm
+#-----------------------------------------------------
+
+
+
