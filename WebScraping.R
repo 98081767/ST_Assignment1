7ee746117bc35877f495312e0e7925bb29750185
98081767 <98081767@student.uts.edu.au>
2018-08-12 22:01:32 +1000

    Added function

--- WebScraping.R
+++ WebScraping.R
@@ -46,47 +46,71 @@ install.packages("magrittr")
 library(magrittr)
 
 
-weeklyBoxOffice = page_html %>% 
+myColNames = c("ThisWeek", "LastWeek", "Title", "Studio", "WeeklyGross", "PctChange", "TheatreCount", "TheatreChange", "Average", "TotalGross", "Budget", "WeekNum")
+
+
+weeklyBoxOffice = page_html %>%
   html_nodes("table") %>%
-  extract2(5) %>%
+  extract2(5) %>% 
   html_table() %>%
-  setNames(mycolnames) %>%
-  filter(row_number()==1 && row_number()==n())
+  setNames(myColNames) %>%
+  filter(row_number()!=1) %>% 
+  filter(row_number()!=n())
+
+
+#test errors
+
+  tryCatch(
+    {
+    page_html %>%
+      html_nodes("table") %>%
+      extract2(5) %>% 
+      html_table() %>%
+      setNames(myColNames) %>%
+      filter(row_number()!=1) %>% 
+      filter(row_number()!=n())
+    }
+    ,
+    error=function(e) return(NULL)
+  )
 
 
 
-
-
-mycolnames = colnames(weeklyBoxOffice)
-
-
+#------------------
+# make it a function.
 
 
 
-colnames(weeklyBoxOffice) = weeklyBoxOffice[1,]
+getWeeklyBoxOffice = function(theYear, theWeek) {
 
+  tryCatch(
+    {  
+      base_url = "https://www.boxofficemojo.com/weekly/chart/"
+      query_params = list(yr=theYear, wk=theWeek)
+      
+      myResp = GET(url = base_url, query=query_params)
+      
+      myPage_html = read_html(myResp)
+      
+      myColNames = c("ThisWeek", "LastWeek", "Title", "Studio", "WeeklyGross", "PctChange", "TheatreCount", "TheatreChange", "Average", "TotalGross", "Budget", "WeekNum")
+      
+        myWeeklyBoxOffice = myPage_html %>%
+        html_nodes("table") %>%
+        extract2(5) %>%
+        html_table() %>%
+        setNames(myColNames) %>%
+        filter(row_number()!=1) %>% 
+        filter(row_number()!=n()) 
+      
+        return(myWeeklyBoxOffice)
+    },
+        error=function(e) return(NULL)
+  )
+}
+         
+        
 
+df = getWeeklyBoxOffice("2018", "31")
 
-
-
-#page_html = read_html(xml_text(resp_html))
-#install.packages("XML")
-#library(XML)
-
-
-# saveXML(page_html[[2]], file="resp_html.html", indent=TRUE)
-# 
-# install.packages("rio")
-# library(rio)
-# 
-# export(page_html,
-#        file="resp_html.html",
-#        format="html"
-# )
-
-# install.packages("R2HTML")
-# library(R2HTML)
-# 
-# HTML(page_html[[2]], file="resp_html.html")
 
 
