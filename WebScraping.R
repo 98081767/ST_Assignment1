a30c8e2bd1e684f2b951f40291513e4ad526a9b6
98081767 <98081767@student.uts.edu.au>
2018-08-12 20:24:10 +1000

    extracting table

--- WebScraping.R
+++ WebScraping.R
@@ -4,5 +4,89 @@
 # Web scrapting of movie sales - from https://www.boxofficemojo.com/weekly/chart/?yr=2018&wk=31&p=.htm
 #-----------------------------------------------------
 
+install.packages("pacman")
+library(pacman)
+
+# install.packages("httr")
+# library(httr)
+# 
+
+
+p_load(httr, rvest, XML, dplyr)
+
+getYear = "2018"
+getWeek = "31"
+
+
+base_url = "https://www.boxofficemojo.com/weekly/chart/"
+query_params = list(yr=getYear, wk=getWeek)
+
+resp = GET(url = base_url, query=query_params)
+
+str(resp)
+
+resp_html = content(resp)
+
+
+#using rvest functions
+# install.packages("rvest")
+# library(rvest)
+
+page_html = read_html(resp)
+
+write_xml(page_html, file="resp_html.html")
+
+title = page_html %>% 
+  html_nodes("title") %>%
+  html_text()
+
+
+#need to install this to access the list using extract2 function
+install.packages("magrittr")
+library(magrittr)
+
+
+weeklyBoxOffice = page_html %>% 
+  html_nodes("table") %>%
+  extract2(5) %>%
+  html_table() %>%
+  setNames(mycolnames) %>%
+  filter(row_number()==1 && row_number()==n())
+
+
+
+
+
+mycolnames = colnames(weeklyBoxOffice)
+
+
+
+
+
+colnames(weeklyBoxOffice) = weeklyBoxOffice[1,]
+
+
+
+
+
+#page_html = read_html(xml_text(resp_html))
+#install.packages("XML")
+#library(XML)
+
+
+# saveXML(page_html[[2]], file="resp_html.html", indent=TRUE)
+# 
+# install.packages("rio")
+# library(rio)
+# 
+# export(page_html,
+#        file="resp_html.html",
+#        format="html"
+# )
+
+# install.packages("R2HTML")
+# library(R2HTML)
+# 
+# HTML(page_html[[2]], file="resp_html.html")
 
 
